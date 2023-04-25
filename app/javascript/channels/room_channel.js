42bf1dfb2656dbe360eca87d90a299f05ca4a4ee
Charlie Massry <me@charliemassry.com>
2023-04-24 22:22:41 -0400

    add email and unique block color to position

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -16,22 +16,63 @@ export default consumer.subscriptions.create({ channel: "RoomChannel", id: id}, 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
       if (data.sent_by !== currentUserEmail) {
-          var existingBlock = document.querySelector(".other_user_block[data-email='" + data.sent_by + "']");
-          var block;
-          if (existingBlock) {
-              block = existingBlock
+          var existingBlockContainer = document.querySelector(".other_user_block_container[data-email='" + data.sent_by + "']");
+          var container;
+          if (existingBlockContainer) {
+              container = existingBlockContainer;
           } else {
-              block = document.createElement("div");
-              block.className = "other_user_block";
+              container = document.createElement("div");
+              container.className = "other_user_block_container";
+              container.dataset.email = data.sent_by;
+
+              var block = document.createElement("div");
+              container.append(block);
+
+              var emailBlock = document.createElement("div");
+              emailBlock.innerText = data.sent_by;
+              container.append(emailBlock);
+
               block.dataset.email = data.sent_by;
               block.style.width = "10px";
               block.style.height = "10px";
-              block.style.position = "absolute";
-              block.style.backgroundColor = "black";
-              document.body.appendChild(block);
+              container.style.position = "absolute";
+              block.style.backgroundColor = randomColor();
+
+              document.body.appendChild(container);
           }
-          block.style.top = data.y + "px";
-          block.style.left = data.x + "px";
+          container.style.top = data.y + "px";
+          container.style.left = data.x + "px";
       }
   }
 });
+
+var webColors = [
+    "#FF0000", // Red
+    "#00FF00", // Green
+    "#0000FF", // Blue
+    "#FFFF00", // Yellow
+    "#FF00FF", // Magenta
+    "#00FFFF", // Cyan
+    "#800000", // Maroon
+    "#008000", // Olive
+    "#000080", // Navy
+    "#808000", // Olive Drab
+    "#800080", // Purple
+    "#008080", // Teal
+    "#C0C0C0", // Silver
+    "#808080", // Gray
+    "#FFA500", // Orange
+    "#FFC0CB", // Pink
+    "#800000", // Brown
+    "#FFD700", // Gold
+    "#00FF7F", // Spring Green
+    "#FF69B4" // Hot Pink
+];
+
+function randomColor() {
+    var color = webColors[Math.floor(Math.random() * webColors.length)];
+
+    webColors = webColors.filter( c => c !== color);
+
+    return color;
+}
